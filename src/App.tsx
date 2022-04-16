f18afe810761359af63f1e8d23a955ec216ad2b5
ChingChen Liew <liewchingchen@gmail.com>
2022-04-16 11:18:45 +0800

    added Ant Design library

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
+import { Divider } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+        probare, quae sunt a te dicta? Refert tamen, quo modo.
+      </p>
+      <Divider />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+        probare, quae sunt a te dicta? Refert tamen, quo modo.
+      </p>
+      <Divider dashed />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+        probare, quae sunt a te dicta? Refert tamen, quo modo.
+      </p>
+    </div >
   );
 }
 
