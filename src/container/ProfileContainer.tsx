34806e2534e7afc87427d4166407da24b81f3eae
ChingChen Liew <liewchingchen@gmail.com>
2022-04-17 12:39:48 +0800

    added google analytic 4 (GA4)

--- src/container/ProfileContainer.tsx
+++ src/container/ProfileContainer.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import ProfileCard from '../component/ProfileCard';
-import { ChingChenProfileData } from '../data/chingchen-profile'; 
+import { ChingChenProfileData } from '../data/chingchen-profile';
 import { ProfileCardProps } from '../props/ProfileCardProps';
 import './ProfileContainer.css'
+import ReactGA from "react-ga4";
 
-const ProfileContainer = () => { 
+const ProfileContainer = () => {
+  const SendViewMoreDetailsEventToGA4 = () => {
+    // Send a ViewDetail event to GA4.
+    ReactGA.event({
+      category: "view_detail",
+      action: "click_more_detail"
+    });
+  }
+
   const myProfile: ProfileCardProps = {
     User: ChingChenProfileData.Info,
     Detail: {
       BackgroundStories: ChingChenProfileData.Background,
       CareerJourney: ChingChenProfileData.Career,
       Skillsets: ChingChenProfileData.Skillsets
-    }
+    },
+    SendAnalyticEventForViewMoreDetail: SendViewMoreDetailsEventToGA4
   };
 
   return (
-    <section id="profileContainer">      
+    <section id="profileContainer">
       <ProfileCard {...myProfile} />
     </section>
   );
